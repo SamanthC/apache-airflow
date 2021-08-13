f213b58fa9f0eef848e24878d15f4a7868b347be
Samanth Chinta <samanth.chinta@gmail.com>
2021-08-13 11:12:47 +0200

    add job on pipeline_ml_staging

--- airflow/dags/pipeline_ml_staging.py
+++ airflow/dags/pipeline_ml_staging.py
@@ -9,7 +9,8 @@ from google.protobuf.duration_pb2 import Duration
 from airflow.utils import trigger_rule
 from airflow.providers.google.cloud.operators.dataproc import(
 	DataprocCreateClusterOperator,
-	DataprocDeleteClusterOperator
+	DataprocDeleteClusterOperator,
+	DataprocSubmitPySparkJobOperator
 )
 
 DAG_NAME = os.path.basename(__file__).replace('.py', '')
@@ -22,12 +23,12 @@ CLUSTER_CONFIG = {
 	'master_config': {
 		'num_instances': 1,
 		'machine_type_uri': 'n1-standard-2',
-		'disk_config': {'boot_disk_type': 'pd-standard', 'boot_disk_size_gb': 1024}
+		'disk_config': {'boot_disk_type': 'pd-standard', 'boot_disk_size_gb': 512}
 	},
 	'worker_config': {
 		'num_instances': 2,
 		'machine_type_uri': 'n1-standard-2',
-		'disk_config': {'boot_disk_type': 'pd-standard', 'boot_disk_size_gb': 1024}	
+		'disk_config': {'boot_disk_type': 'pd-standard', 'boot_disk_size_gb': 512}	
 	},
 	'lifecycle_config': {
 		'idle_delete_ttl': Duration(seconds=3600)
@@ -52,10 +53,21 @@ with DAG(DAG_NAME, default_args=default_args, schedule_interval='0 5 * * 1') as 
 		cluster_config=CLUSTER_CONFIG
 	)
 
+	task_job = DataprocSubmitPySparkJobOperator(
+		task_id='submit_job',
+		job_name='query_user_events',
+		cluster_name=CLUSTER_NAME_TEMPLATE + '{{ ds-nodash }}',
+		main='gs://{}/scripts/create_events_data_file.py'.format(BUCKET),
+		dataproc_jars=['gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar'],
+		arguments=['{{ ds }}', '{{ next_ds }}']
+	)
+
+
 	task_delete_dataproc = DataprocDeleteClusterOperator(
 		task_id='delete_dataproc',
 		cluster_name=CLUSTER_NAME_TEMPLATE + "{{ ds_nodash }}",
 		trigger_rule=trigger_rule.TriggerRule.ALL_DONE
 	)
 
-	task_create_dataproc.set_downstream(task_delete_dataproc)
+	task_create_dataproc.set_downstream(task_job)
+	task_job.set_downstream(task_delete_dataproc)
